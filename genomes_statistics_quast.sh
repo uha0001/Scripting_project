91638238c5e72b9e6d363913cab667201f0d29c8
Md Jahangir Alam <mza0150@auburn.edu>
2020-04-15 15:39:36 -0500

    install and run quast

--- genomes_statistics_quast.sh
+++ genomes_statistics_quast.sh
@@ -1,1 +1,10 @@
 
+#Installation guidelines: http://quast.sourceforge.net/install.html
+# This program can be used in Mac/Virtualbox/Linux server (ASC):
+#To install in ASC:
+wget https://downloads.sourceforge.net/project/quast/quast-5.0.2.tar.gz
+tar -xzf quast-5.0.2.tar.gz
+cd quast-5.0.2
+# Copy all the genomes fna files into quast-5.0.2 directory to run quast
+# To run quast and save outputs in output_dir:
+quast.py *.fna -o output_dir
