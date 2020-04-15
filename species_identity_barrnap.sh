295cabdb4a96abf8fe54ad0c328001cfdd0d8122
Md Jahangir Alam <mza0150@auburn.edu>
2020-04-15 14:41:48 -0500

    installling barrnap in ASC account

--- species_identity_barrnap.sh
+++ species_identity_barrnap.sh
@@ -1,1 +1,10 @@
 
+# Install Barrnap (https://github.com/tseemann/barrnap) in ASC:
+# Activate anaconda module in ASC to create an unique environment:
+module load anaconda/2-4.2.0_cent
+# Create a conda environment:
+conda create -n barrnap_ENV
+# Activate newly created barrnap environment:
+source activate barrnap_ENV
+# Install Barrnap into activated barrnap environment
+conda install -c bioconda -c conda-forge barrnap
