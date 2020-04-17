aba7cf30c2927ddb5dcab6a6230cf537c34935cc
Natalia Rivera R <nzr0037@auburn.edu>
2020-04-17 16:55:30 -0500

    Script to generate data for rpsblast

---
+++ rpsblast.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd ~/Scripting_project/Prokka
+cat ~/Scripting_project/Genome_Files/*.fna >> Data_base.frn
+
+makeblastdb -dbtype nucl -in Data_base.frn -out Data_Base.frn
+
+#echo -e ">hello\nAAA\n>world\nATGCA" | awk '/^>/ {fout=sprintf("%s.faa",substr($0,2));}{print >> fout;}' *.fasta
+
