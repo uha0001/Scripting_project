ec3f06690edd061b2d1c1fbd051344fb8507f842
uha0001 <uha0001@auburn.edu>
2020-04-17 14:59:38 -0500

    for all fna files

--- horizontal_gene_transfer.sh
+++ horizontal_gene_transfer.sh
@@ -5,10 +5,13 @@ cp ~/Scripting_project/Genome_Files/*.fna .
 
 #Raw data files from step 1 should be moved into the alien_hunter-1.7. Then the following command can be executed.
 #Split the fasta files by '>' into a file each containing one sequence, and have the name of that file be the ID
-echo -e ">hello\nAAA\n>world\nATGCA" | awk '/^>/ {fout=sprintf("%s.fasta",substr($0,2));}{print >> fout;}' *.fasta
+#echo -e ">hello\nAAA\n>world\nATGCA" | awk '/^>/ {fout=sprintf("%s.fasta",substr($0,2));}{print >> fout;}' *.fna
 
 
 #Alien_hunter calculates the percentage of each variable region depending on horizontally transferred or not.
 #usage alien_hunter file_name.fasta output_filename
-#alien_hunter *.fna Output_Filename
+for filename in ./*.fna
+do
+alien_hunter *.fna Output_Filename
 
+done
