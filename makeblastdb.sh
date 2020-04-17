d09ad0dfb059039adf6ea251e004002398c95042
natbio12 <60452235+natbio12@users.noreply.github.com>
2020-04-17 18:15:55 -0500

    Update makeblastdb.sh

--- makeblastdb.sh
+++ makeblastdb.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 cd ~/Scripting_project/Prokka
-cat ~/Scripting_project/Genome_Files/*.fna >> Data_base.frn
+cat ~/Scripting_project/Genome_Files/*.fna >> Data_base.fa
 
-makeblastdb -dbtype nucl -in Data_base.frn -out Data_Base.frn
+makeblastdb -dbtype nucl -in Data_base.fa -out Data_Base.fa
 
 mkdir Individual_query
 
-echo -e ">hello\nAAA\n>world\nATGCA" | awk '/^>/ {fout=sprintf("%s.faa",substr($0,2));}{print >> fout;}' *.fna
+echo -e ">hello\nAAA\n>world\nATGCA" | awk '/^>/ {fout=sprintf("%s.fasta",substr($0,2));}{print >> fout;}' *.fna
 
-mv ~/Scripting_project/Prokka/*.faa ~/Scripting_project/Prokka/Individual_query/
+mv ~/Scripting_project/Prokka/*.fasta ~/Scripting_project/Prokka/Individual_query/
