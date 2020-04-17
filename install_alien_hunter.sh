5039c0869e941e88657898083f90355578d26deb
uha0001 <uha0001@auburn.edu>
2020-04-17 11:21:33 -0500

    installing the alien hunter

---
+++ install_alien_hunter.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#download the alien_hunter
+wget ftp://ftp.sanger.ac.uk/pub/resources/software/alien_hunter/alien_hunter.tar.gz
+
+#unzip the file in the folder you are working on
+tar xvzf alien_hunter.tar.gz
