5990bb8874b8aea0ad92bf085c57c2ba3c544bda
uha0001 <uha0001@auburn.edu>
2020-04-18 00:53:35 -0500

    final loop structure

--- horizontal_gene_transfer.sh
+++ horizontal_gene_transfer.sh
@@ -12,6 +12,6 @@ cp ~/Scripting_project/Genome_Files/*.fna .
 #usage alien_hunter file_name.fasta output_filename
 for filename in ./*.fna
 do
-alien_hunter *.fna Output_Filename
+alien_hunter $filename $filename_output_Filename -a
 
 done
