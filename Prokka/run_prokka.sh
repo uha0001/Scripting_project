9490bdcfb28ea9e70461143db33667dccb7900dd
basu-kafle <bzk0055@auburn.edu>
2020-04-17 21:07:30 -0500

    script_edited

--- Prokka/run_prokka.sh
+++ Prokka/run_prokka.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
+#load anaconda
+module load anaconda/2-4.2.0_cent
+
+#activate prokka environment 
+source activate prokka
 
 
+#copy genome sequence into the directory
 cp ~/Scripting_project/Genome_Files/*.fna .
 
-
+#time to run prokka
 for k in *.fna
 do
 prokka $k --outdir "$k".prokka_annotation --prefix $k
 done
 
-#removing the genome sequence files
+#this will generate output with each genome into a seperate directory
+
+
+#Once prokka completes its work 
+# now it is time to remove genome sequence from directory
 rm *.fna
