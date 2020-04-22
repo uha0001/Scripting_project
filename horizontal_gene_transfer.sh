28b0955d1380a8a4e3d4b506b33ba1d92880e7db
uha0001 <51422853+uha0001@users.noreply.github.com>
2020-04-22 17:08:44 -0500

    analysis for horizontal gene transfer

--- horizontal_gene_transfer.sh
+++ horizontal_gene_transfer.sh
@@ -3,7 +3,7 @@
 cd ~/Scripting_project/alien_hunter-1.7
 cp ~/Scripting_project/Genome_Files/*.fna .
 
-#usage alien_hunter file_name.fasta output_filename
+#usage alien_hunter file_name.fna output_filename
 for filename in ./*.fna
 do
 alien_hunter $filename output_filename
